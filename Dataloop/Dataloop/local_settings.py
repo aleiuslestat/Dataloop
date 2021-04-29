70ae83d388b4ce001830226f79302ad8af1129e7
aleiuslestat <aleius.lestat@gmail.com>
2021-04-28 20:25:21 -0500

    Update local_settings.py

--- Dataloop/Dataloop/local_settings.py
+++ Dataloop/Dataloop/local_settings.py
@@ -13,8 +13,8 @@ DATABASES = {
         'NAME'    : 'postgres',
         'USER'    : 'postgres',
         'PASSWORD': 'postgres',
-         'HOST'   : 'db_postgres',
-#        'HOST'   : 'localhost',
+#         'HOST'   : 'db_postgres',
+         'HOST'   : 'localhost',
         'PORT'    : 5432,
     }
 }
